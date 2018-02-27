b3499e054c4901a12dd934e0007fde40fc725912
AmyLGalles <9685081+AmyLGalles@users.noreply.github.com>
2018-02-26 19:24:20 -0800

    fixed betty on laptop

--- helperprint.c
+++ helperprint.c
@@ -1,88 +1,88 @@
 #include "holberton.h"
-/**                                                                                                
- *_printchar - function for printing a character to standard output                                
- *                                                                                                 
- *@args: arguments passed from main function                                                       
- *@buffer: temporary storage of values                                                             
- *@offset: pointer position in string buffer                                                       
- *                                                                                                 
- *Return: value of char (1)                                                                        
+/**                                                                                   
+ *_printchar - function for printing a character to standard output                   
+ *                                                                                    
+ *@args: arguments passed from main function                                          
+ *@buffer: temporary storage of values                                                
+ *@offset: pointer position in string buffer                                          
+ *                                                                                    
+ *Return: value of char (1)                                                           
  */
 int _printchar(va_list args, char *buffer, int offset)
 {
-  char a;
+        char a;
 
-  a = va_arg(args, int);
-  buffer[offset] = a;
-  return (1);
+        a = va_arg(args, int);
+        buffer[offset] = a;
+        return (1);
 }
-/**                                                                                                
- *_printstring - prints a string of characters to buffer                                           
- *                                                                                                 
- *@args: arguments passed from main function                                                       
- *@buffer: temporary storage of values                                                             
- *@offset: pointer position in string buffer                                                       
- *                                                                                                 
- *Return: length of string                                                                         
+/**                                                                                   
+ *_printstring - prints a string of characters to buffer                              
+ *                                                                                    
+ *@args: arguments passed from main function                                          
+ *@buffer: temporary storage of values                                                
+ *@offset: pointer position in string buffer                                          
+ *                                                                                    
+ *Return: length of string                                                            
  */
 int _printstring(va_list args, char *buffer, int offset)
 {
-  char *s;
-  int i = 0, j = 0;
+        char *s;
+        int i = 0, j = 0;
 
-  s = va_arg(args, char *);
-  while (s[i] != 0)
-    i++;
-  for (j = 0; j < i; j++)
-    buffer[offset + j] = s[j];
-  return (i);
+        s = va_arg(args, char *);
+        while (s[i] != 0)
+                i++;
+        for (j = 0; j < i; j++)
+                buffer[offset + j] = s[j];
+        return (i);
 }
-/**                                                                                                
- *_printdecimal - print base10 numbers passed from main function                                   
- *                                                                                                 
- *@args: arguments stored in va_list                                                               
- *@buffer: temporary storage of values to print                                                    
- *@offset: position of va_args in argument list                                                    
- *                                                                                                 
- *Return: 0 if success, 1 if i == 0                                                                
+/**                                                                                   
+ *_printdecimal - print base10 numbers passed from main function                      
+ *                                                                                    
+ *@args: arguments stored in va_list                                                  
+ *@buffer: temporary storage of values to print                                       
+ *@offset: position of va_args in argument list                                       
+ *                                                                                    
+ *Return: 0 if success, 1 if i == 0                                                   
  */
 int _printdecimal(va_list args, char *buffer, int offset)
 {
-  int i;
-  int x = 0;
-  int isnegative;
-  char itoa[1024];
-  int remainder = 0;
-  int temp;
-  int base = 10;
+        int i;
+        int x = 0;
+        int isnegative;
+        char itoa[1024];
+        int remainder = 0;
+        int temp;
+        int base = 10;
 
-  i = va_arg(args, int);
-  if (i == 0)
-    {
-      buffer[offset] = '0';
-      return (1);
-    }
-  if (i < 0 && base == 10)
-    {
-      isnegative = 1;
-      i *= -1;
-    }
-  while (i != 0)
-    {
-      remainder = i % base;
-      itoa[x++] = (remainder > 9) ? (remainder - 10) + 'a' : remainder + '0';
-      i /= base;
-    }
-  if (isnegative)
-    {
-      itoa[x++] = '-';
-    }
-  for (i = 0; i < x / 2; i++)
-    {
-      temp = itoa[i];
-      itoa[i] = itoa[x - i - 1];
-      itoa[x - i - 1] = temp;
-    }
-  for (i = 0; i < x; i++)
-    buffer[offset + i] = itoa[i];
+        i = va_arg(args, int);
+        if (i == 0)
+        {
+                buffer[offset] = '0';
+                return (1);
+        }
+        if (i < 0 && base == 10)
+        {
+                isnegative = 1;
+                i *= -1;
+        }
+        while (i != 0)
+        {
+                remainder = i % base;
+                itoa[x++] = (remainder > 9) ? (remainder - 10) + 'a' : remainder + '0';
+                i /= base;
+        }
+        if (isnegative)
+        {
+                itoa[x++] = '-';
+        }
+        for (i = 0; i < x / 2; i++)
+        {
+                temp = itoa[i];
+                itoa[i] = itoa[x - i - 1];
+                itoa[x - i - 1] = temp;
+        }
+        for (i = 0; i < x; i++)
+                buffer[offset + i] = itoa[i];
 }
