825ac63b087c3eff25ea5408370e9c9dc3872c06
AmyLGalles <9685081+AmyLGalles@users.noreply.github.com>
2018-02-26 19:50:48 -0800

    Betty checking printf.c
    moved va_start(args, format); after declarations so that a blank line would be present, removed comments

--- printf.c
+++ printf.c
@@ -1,44 +1,44 @@
 #include "holberton.h"
-/**
- * _printf - prints a formatted string
- * @format: format specifiers for the string
- * @...: variadic arguments
- * Return: 0 on successful run
+/**                                                                                   
+ * _printf - prints a formatted string                                                
+ * @format: format specifiers for the string                                          
+ * @...: variadic arguments                                                           
+ * Return: 0 on successful run                                                        
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	va_start(args, format);
+ 	va_list args;
 	int i = 0, x = 0, c = 0;
 	char buffer[1024];
-	Match *match = matchinit();	
+	Match *match = matchinit();
+
+        va_start(args, format);
 
 	for (i = 0; format[i] != 0; i++)
 	{
-		if (format[i] == '%')
+         	if (format[i] == '%')
 		{
-			for (x = 0; match[x].f != NULL; x++)
+                 	for (x = 0; match[x].f != NULL; x++)
 			{
-				if (format[i + 1] == *match[x].c)
+                         	if (format[i + 1] == *match[x].c)
 				{
-					c += match[x].f(args, buffer, c);
+                                 	c += match[x].f(args, buffer, c);
 					i++;
-					break;
+                                        break;
 				}
-				else if (format[i + 1] == '%')
+                                else if (format[i + 1] == '%')
 				{
-					buffer[c++] = format[i++ + 1];
+                                 	buffer[c++] = format[i++ + 1];
 					break;
 				}
 			}
 		}
-		else
+                else
 		{
-			buffer[c++] = format[i]; 	
-		}
-		
+                 	buffer[c++] = format[i];
+                }
 	}
-	write(1, buffer, c);
+        write(1, buffer, c);
 	va_end(args);
 	free(match);
 	return (c);
