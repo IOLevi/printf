1cb5a0c8467a3f3d8e4384cca8ce1e3c9ec2a86d
AmyLGalles <suzyhendrix@gmail.com>
2018-02-27 04:24:29 +0000

    betty checked initialize.c

--- initialize.c
+++ initialize.c
@@ -8,20 +8,20 @@ Match *matchinit(void)
 	if (!match)
 		return (NULL);
 
-	match[0].c= "c";
-	match[0].f = _printchar; 
-	
+	match[0].c = "c";
+	match[0].f = _printchar;
+
 	match[1].c = "s";
-	match[1].f = _printstring; 
-	
+	match[1].f = _printstring;
+
 	match[2].c = "i";
-	match[2].f = _printdecimal; 
-	
+	match[2].f = _printdecimal;
+
 	match[3].c = "d";
 	match[3].f = _printdecimal;
 
 	match[4].c = NULL;
-	match[4].f = NULL;	
+	match[4].f = NULL;
 
 	return (match);
 }
