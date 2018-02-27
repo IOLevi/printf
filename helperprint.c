0294f2341e05df2b5b067c2de9203b2191f50b33
AmyLGalles <suzyhendrix@gmail.com>
2018-02-27 04:20:49 +0000

    betty checked helperprint.c

--- helperprint.c
+++ helperprint.c
@@ -1,9 +1,12 @@
 #include "holberton.h"
-/* 1. comments for every function
- * 2. remove all c++ style // comments
- * 3. space between declarations and code
- * 4. 40 lines per function
- * 5. stufffff.
+/**
+ *_printchar - function for printing a character to standard output
+ *
+ *@args: arguments passed from main function
+ *@buffer: temporary storage of values
+ *@offset: pointer position in string buffer
+ *
+ *Return: value of char (1)
  */
 int _printchar(va_list args, char *buffer, int offset)
 {
@@ -11,27 +14,38 @@ int _printchar(va_list args, char *buffer, int offset)
 
 	a = va_arg(args, int);
 	buffer[offset] = a;
-
 	return (1);
-	//write(1, &a, 1);
-	
 }
-
+/**
+ *_printstring - prints a string of characters to buffer
+ *
+ *@args: arguments passed from main function
+ *@buffer: temporary storage of values
+ *@offset: pointer position in string buffer
+ *
+ *Return: length of string
+ */
 int _printstring(va_list args, char *buffer, int offset)
 {
 	char *s;
 	int i = 0, j = 0;
 
 	s = va_arg(args, char *);
-
 	while (s[i] != 0)
 		i++;
 	for (j = 0; j < i; j++)
 		buffer[offset + j] = s[j];
-	
 	return (i);
-	//write(1, s, i);
 }
+/**
+ *_printdecimal - print base10 numbers passed from main function
+ *
+ *@args: arguments stored in va_list
+ *@buffer: temporary storage of values to print
+ *@offset: position of va_args in argument list
+ *
+ *Return: 0 if success, 1 if i == 0
+ */
 int _printdecimal(va_list args, char *buffer, int offset)
 {
 	int i;
@@ -43,41 +57,32 @@ int _printdecimal(va_list args, char *buffer, int offset)
 	int base = 10;
 
 	i = va_arg(args, int);
-
 	if (i == 0)
 	{
 		buffer[offset] = '0';
 		return (1);
-	}	
-
+	}
 	if (i < 0 && base == 10)
 	{
 		isnegative = 1;
 		i *= -1;
 	}
-
 	while (i != 0)
 	{
 		remainder = i % base;
 		itoa[x++] = (remainder > 9) ? (remainder - 10) + 'a' : remainder + '0';
-		i /= base; 
+		i /= base;
 	}
-
 	if (isnegative)
 	{
 		itoa[x++] = '-';
 	}
-	
-	//reverse the itoa array
-	
 	for (i = 0; i < x / 2; i++)
 	{
 		temp = itoa[i];
 		itoa[i] = itoa[x - i - 1];
 		itoa[x - i - 1] = temp;
 	}
-	
 	for (i = 0; i < x; i++)
 		buffer[offset + i] = itoa[i];
 }
-
