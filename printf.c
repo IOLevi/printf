fc3e169593dfac81e7c758c9d4c26a05a552c4bc
IOLevi <337@holbertonschool.com>
2018-02-28 01:53:51 +0000

    accounting for %

--- printf.c
+++ printf.c
@@ -8,7 +8,7 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int i = 0, x = 0, c = 0;
+	int i = 0, x = 0, c = 0, hit = 0;
 	char buffer[1024];
 	Match match[15];
 
@@ -16,6 +16,7 @@ int _printf(const char *format, ...)
 	matchinit(match);
 	for (i = 0; format[i] != 0; i++)
 	{
+		hit = 0;
 		if (format[i] == '%')
 		{
 			for (x = 0; match[x].f != NULL; x++)
@@ -24,26 +25,24 @@ int _printf(const char *format, ...)
 				{
 					c += match[x].f(args, buffer, c);
 					i++;
+					hit = 1;
 					break;
 				}
 				else if (format[i + 1] == '%')
 				{
 					buffer[c++] = format[i++ + 1];
+					hit = 1;
 					break;
 				}
 			}
-			buffer[c++] = format[i];
+			if (!hit)
+				buffer[c++] = format[i];
 		}
 		else
-		{
 			buffer[c++] = format[i];
-		}
-
 	}
 	if (c == 0)
-	{
 		return (-1);
-	}
 	write(1, buffer, c);
 	va_end(args);
 	return (c);
